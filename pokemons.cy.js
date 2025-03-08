d92327e50266a954f4fa73c9823c9ae5700fe40b
yuliya-kyznecova-qa <k1ruhina63@yandex.ru>
2025-03-08 08:33:30 +0400

    Update pokemons.cy.js

--- pokemons.cy.js
+++ pokemons.cy.js
@@ -1,8 +1,8 @@
 describe('Покупка аватара', function () {                                // название набора тестов
     it('e2e тест на покупку нового аватара для тренера', function () {   // название теста
          cy.visit('https://pokemonbattle.ru/');                          // переходим на сайт https://pokemonbattle.ru/
-         cy.get('input[type="email"]').type('k1ruhina63@yandex.ru');                   // вводим логин
-         cy.get('input[type="password"]').type('uKmk100279');               // вводим пароль
+         cy.get('input[type="email"]').type('a63@yandex.ru');                   // вводим логин
+         cy.get('input[type="password"]').type('uK9');               // вводим пароль
          cy.get('button[type="submit"]').click();                        // нажимаем кнопку Подтвердить
          cy.wait(2000);
          cy.get('.header__container > .header__id').click({ force: true }); // Клик в шапке на аву тренера
